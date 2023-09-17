ecb1eb5dbf14274f6170ba5dae15d8847fe6f8c8
Danmar Cornelio Nacario <dcnacario08@gmail.com>
2023-09-17 19:24:35 +0800

    ADDING scores
    implementing a scoring system for the game

--- external.js
+++ external.js
@@ -1,5 +1,8 @@
 let computerChoice = "";
 let message = "";
+let playerScore = 0;
+let computerScore = 0;
+let draw = 0;
 
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3);
@@ -18,24 +21,32 @@ function getComputerChoice(){
 function playRound (playerChoice,computerChoice){
     let playerChoiceNotSensitive = playerChoice.toLowerCase();
     if (playerChoiceNotSensitive == "rock" && computerChoice == "scissors"){
+        playerScore++;
         return "You Win, Rock beats Scissors";
     }
     else if(playerChoiceNotSensitive == "scissors" && computerChoice == "rock"){
+        computerScore++;
         return  "You Lose, Scissors beats Rock";
     }
     else if(playerChoiceNotSensitive == "paper" && computerChoice == "rock"){
+        playerScore ++;
         return "You Won!, Paper beats Rock";
     }
     else if(playerChoiceNotSensitive == "rock" && computerChoice == "paper"){
+        computerScore++;
         return "You Lose!, Paper beats Rock";
     }
     else if(playerChoiceNotSensitive == "scissors" && computerChoice == "paper"){
+        playerScore++;
         return "You Won!, Scissors beats Paper";
+
     }
     else if(playerChoiceNotSensitive == "paper" && computerChoice == "scissors"){
+        computerScore++;
         return "You Lose!, Paper beats Scissors";
     }
     else if(playerChoice.length == computerChoice.length) {
+        draw++;
         return "Draw";
     }
     else {
@@ -44,11 +55,14 @@ function playRound (playerChoice,computerChoice){
 }
 
 function game(){
-    let playerChoice = prompt("Choices: Rock, Paper, Scissor", "");
+    let playerChoice = prompt("Choices: Rock, Paper, Scissors", "");
     for(i = 0 ; i < 5 ; i++){
         computerChoice = getComputerChoice();
         console.log(computerChoice)
         console.log(playRound(playerChoice,computerChoice));
+        console.log("Player Score: "+playerScore);
+        console.log("Computer Score: "+computerScore);
+        console.log("Draws: "+draw);
     }
 }
 game();
